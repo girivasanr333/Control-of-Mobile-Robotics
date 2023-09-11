89a64d4abcab5f74c97e2f2ee1ee9e2f9cdc8e14
girivasanr333 <108106100+girivasanr333@users.noreply.github.com>
2023-09-11 00:39:50 -0400

    Update test_3.py

--- Lab1/test_3.py
+++ Lab1/test_3.py
@@ -4,6 +4,9 @@ import pigpio
 pigpi = pigpio.pi() 
         
 controller = robot_controller.control(pi=pigpi)
+
+# The robot moves 10mm
 controller.straight(1000)
+
 # controller.set_speed_r(0)
 # controller.set_speed_l(0)
