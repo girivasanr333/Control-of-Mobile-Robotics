d17b5bd9b46d823c4e1be9bcd1d20f812ffdad32
girivasanr333 <108106100+girivasanr333@users.noreply.github.com>
2023-10-19 21:53:15 -0400

    Update lab2liz.py

--- Mobile_Robotics_Lab 2/lab2liz.py
+++ Mobile_Robotics_Lab 2/lab2liz.py
@@ -2,26 +2,33 @@ import time
 import robot_controller
 import pigpio
 
-# Initializing the pigpio library which is a library to interface with GPIO on a Raspberry Pi.
+# Initialize the pigpio library
 pigpi = pigpio.pi()
 
-# Initializing our custom robot controller which likely controls motors and reads from sensors.
+# Initialize the robot controller
 controller = robot_controller.control(pi=pigpi)
 
-# Defining speed constants.
-max_speed = 0.4
+max_speed = 0.40
 min_speed = 0.35
 normal_speed = 0.3
 
-# Distance the robot should maintain from walls.
-set_distance = 15
-front_set_distance = 20
-max_distance = 40  # Ideal distance for the robot to decide to make a turn.
+min_front_speed = 0.1
 
-# Proportional constant for the PID controller.
-Kp_side = 0.1
+set_distance = 15 # Set distance from the wall
+front_set_distance = 22 #front wall
+max_distance = 40 #distance between right & left wall
 
-# Function to ensure speed values remain within set bounds.
+Kp_side = 0.1  # Replace with the best Kp value from your tests
+
+
+def get_saturated_speed_front_ds(speed):
+    if(speed > max_speed):
+        return max_speed
+    elif(speed < min_front_speed):
+        return min_front_speed
+    else:
+        return speed
+
 def get_saturated_speed(speed):
     if(speed > max_speed):
         return max_speed
@@ -31,89 +38,118 @@ def get_saturated_speed(speed):
         return speed
     
 
-# Function to make the robot turn until there's no obstacle in the front.
-def turn_for_front_ds(direction="right"):
-    front_distance = controller.get_distance_reading(controller.front_ds)
+
+
+
+def wall_follow_front_ds(direction="right", Kp_side=0.1):
+    """Turns the robot a certain degrees to the specified direction."""
+    print("Inside front wall following " + direction)
+    #front_distance = controller.get_distance_reading( controller.front_ds)
+    front_distance, right_distance, rear_distance, left_distance = controller.get_primary_distance_sensor_readings()
+    l_speed = 0
+    r_speed = 0
+    side_control = 0
+   
 
     while front_distance < max_distance:
         if direction == "right":
-            controller.set_speed_l(0.3)
-            controller.set_speed_r(-0.3)
+            side_control = (right_distance - front_distance) * Kp_side
+            l_speed =get_saturated_speed_front_ds(side_control)
+            #l_speed = 0.3
+            r_speed = get_saturated_speed_front_ds(-1*side_control)
+            
         else:
-            controller.set_speed_l(-0.3)
-            controller.set_speed_r(0.3)
-        front_distance = controller.get_distance_reading(controller.front_ds)
+            side_control = (left_distance - front_distance) * Kp_side
+            l_speed =get_saturated_speed_front_ds(-1*side_control)
+            r_speed = get_saturated_speed_front_ds(side_control)
+            #r_speed = 0.3
+        controller.set_speed_l(l_speed)
+        controller.set_speed_r(r_speed)
+        front_distance = controller.get_distance_reading( controller.front_ds)
 
+   
     controller.set_speed_l(0)
     controller.set_speed_r(0)
+    print("out of front wall following")
 
-# Main function for wall following.
 def wall_following(controller, Kp_side=0.1):
-    print(f"Starting wall following with Kp_side: {Kp_side}")
+    
+    distance_reached = False
 
     start_time = time.time()
     current_time = start_time
+   # while not distance_reached:
+
     l_speed = normal_speed
     r_speed = normal_speed
 
-    # Default direction
     direction = "left"
 
-    # Loop until 60 seconds has passed.
-    while current_time - start_time < 60:
-        # Give the robot a bit of a break between actions.
-        time.sleep(0.1)
-        front_distance, right_distance, _, left_distance = controller.get_primary_distance_sensor_readings()
+    while True:
+    #while not distance_reached:
+        # Retrieve distance sensor readings
+        #time.sleep(0.1)
 
-        # Determine if robot should prioritize the left or right wall.
-        if(right_distance < left_distance):
-            direction = "right"
-        else:
-            direction = "left"
+        front_distance, right_distance, rear_distance, left_distance = controller.get_primary_distance_sensor_readings()
+        print(f"Front Distance: {front_distance}, Right Distance: {right_distance}, Left Distance: {left_distance}")
+        
+        # controller.set_speed_l(l_speed)
+        # controller.set_speed_r(r_speed)
 
-        # Proportional control to adjust speed based on how far robot is from the wall.
+
+    
+        #
+        print(f"Starting wall following in direction: {direction} with Kp_side: {Kp_side}")
+
         if direction == "left":
             error = left_distance - set_distance
-        elif direction == "right":
-            error = right_distance - set_distance
+        # elif direction == "right":
+        #     error = right_distance - set_distance
+        # else:
+        #     error = left_distance - right_distance
+        
+        print(f"Error: {error}")
 
-        side_control = Kp_side * error
+        # Apply proportional control for side walls
+        side_control = Kp_side * (error)
+        print(f"Side Control Adjustment: {side_control}")
 
-        # Adjust robot's speed based on error.
+    
         if direction == "left":
-            l_speed = get_saturated_speed(-1*side_control)
+            #move(controller=controller, l_speed=0.4 - side_control, r_speed=0.4 + side_control,  T=0.005)
+            l_speed =get_saturated_speed(-1*side_control)
             r_speed = get_saturated_speed(side_control)
         elif direction == "right":
-            l_speed = get_saturated_speed(side_control)
+            #move(controller=controller, l_speed=0.4 - side_control, r_speed=0.4 + side_control,  T=0.005)
+            l_speed =get_saturated_speed(side_control)
             r_speed = get_saturated_speed(-1*side_control)
         
-        # If there's an obstacle in the front, decide to turn left or right or make a U-turn.
-        while(front_distance < front_set_distance):
-            '''l_speed = 0.0
-            r_speed = 0.0
-            controller.set_speed_l(0)
-            controller.set_speed_r(0)
-            time.sleep(1)'''
-            if(right_distance > max_distance):
-                turn_for_front_ds("right")
-            elif(left_distance > max_distance):
-                turn_for_front_ds("left")
-            else:
-                turn_for_front_ds("right")
+        
+        # if((left_distance > set_distance)):
+        #     wall_follow_front_ds(direction="left", Kp_side=Kp_side)
+        # elif((front_distance < front_set_distance)):
+        #     wall_follow_front_ds(direction="right", Kp_side=Kp_side)
 
-        # Set the calculated speeds for left and right motors.
         controller.set_speed_l(l_speed)
         controller.set_speed_r(r_speed)
         
         current_time = time.time()
+        print(f"time: {current_time - start_time}")
         
-    # Stop the motors after the loop finishes.
     controller.set_speed_l(0)
     controller.set_speed_r(0)
         
 
-# Test the wall following function.
+# Test the wall following function
+
 start_time = time.time()
 current_time = time.time()
 wall_following(controller=controller, Kp_side=Kp_side)
+
+# while current_time - start_time < 20:
+#     print("following")
+#     wall_following(controller=controller, Kp_side=Kp_side)
+#     time.sleep(2)
+#     current_time = time.time()
+
 
