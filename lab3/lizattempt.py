d5d8bd0abca9fe0b36ddb0291e0d029e812d6566
ElizabethSotomayor <112016889+ElizabethSotomayor@users.noreply.github.com>
2023-11-01 16:40:22 -0400

    Update lizattempt.py

--- lab3/lizattempt.py
+++ lab3/lizattempt.py
@@ -349,7 +349,7 @@ def motion_to_goal():
     obs_detected = False
     front_distance, right_distance, rear_distance, left_distance = controller.get_primary_distance_sensor_readings()
 
-    while not goal_reached:
+     while not goal_reached:
         front_distance, right_distance, rear_distance, left_distance = controller.get_primary_distance_sensor_readings()
         print(f"Front Distance: {front_distance}, Right Distance: {right_distance}, Left Distance: {left_distance}")
         if((front_distance < 20)):
